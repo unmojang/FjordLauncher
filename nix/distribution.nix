5345b0b682bea281a5acc8385f51abfdd3775369
Evan Goode <mail@evangoo.de>
2024-07-20 18:40:32 -0400

    nix: sync changes from nixpkgs
    Brings in changes from the Prism Launcher derivation(s) in nixpkgs, notably
    from https://github.com/NixOS/nixpkgs/pull/321851 and
    https://github.com/NixOS/nixpkgs/pull/303880
    
    Signed-off-by: Evan Goode <mail@evangoo.de>

--- nix/distribution.nix
+++ nix/distribution.nix
@@ -13,8 +13,6 @@
     in {
       inherit
         (ourPackages)
-        fjordlauncher-qt5-unwrapped
-        fjordlauncher-qt5
         fjordlauncher-unwrapped
         fjordlauncher
         ;
@@ -25,37 +23,11 @@
   flake = {
     overlays.default = final: prev: let
       version = builtins.substring 0 8 self.lastModifiedDate or "dirty";
-
-      filteredSelf = inputs.nix-filter.lib.filter {
-        root = ../.;
-        include = [
-          "buildconfig"
-          "cmake"
-          "launcher"
-          "libraries"
-          "program_info"
-          "tests"
-          ../COPYING.md
-          ../CMakeLists.txt
-        ];
-      };
-
-      # common args for prismlauncher evaluations
-      unwrappedArgs = {
-        self = filteredSelf;
-
+    in {
+      fjordlauncher-unwrapped = prev.callPackage ./pkg {
         inherit (inputs) libnbtplusplus;
-        inherit ((final.darwin or prev.darwin).apple_sdk.frameworks) Cocoa;
         inherit version;
       };
-    in {
-      fjordlauncher-qt5-unwrapped = prev.libsForQt5.callPackage ./pkg unwrappedArgs;
-
-      fjordlauncher-qt5 = prev.libsForQt5.callPackage ./pkg/wrapper.nix {
-        fjordlauncher-unwrapped = final.fjordlauncher-qt5-unwrapped;
-      };
-
-      fjordlauncher-unwrapped = prev.qt6Packages.callPackage ./pkg unwrappedArgs;
 
       fjordlauncher = prev.qt6Packages.callPackage ./pkg/wrapper.nix {
         inherit (final) fjordlauncher-unwrapped;
